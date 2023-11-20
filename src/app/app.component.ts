813cb3f8b683a40ec1493659043c72fd11c41f91
Absink <absink@absink.fr>
2023-11-20 11:14:28 +0100

    Add config styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app-crm';
+export class AppComponent implements OnInit {
+
+  public open: boolean = true;
+  title = 'app-demo';
+
+  ngOnInit(): void {
+    this.open = false;
+  }
+
+  public switch(): void {
+    this.open = !this.open;
+  }
 }
