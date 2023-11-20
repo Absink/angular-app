5af44539418d9de16466666ce496c6cb4eaafb67
Absink <absink@absink.fr>
2023-11-20 12:34:44 +0100

    Implémente login Icon

---
+++ src/app/shared/components/icon-login/icon-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-login',
+  templateUrl: './icon-login.component.html',
+  styleUrls: ['./icon-login.component.scss']
+})
+export class IconLoginComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { IconLoginComponent } from './components/icon-login/icon-login.component';
 
 
 
 @NgModule({
   declarations: [
-    IconNavComponent
+    IconNavComponent,
+    IconLoginComponent
   ],
   imports: [
     CommonModule
   ], exports: [
-    IconNavComponent
+    IconNavComponent,
+    IconLoginComponent
   ]
 })
 export class SharedModule { }
