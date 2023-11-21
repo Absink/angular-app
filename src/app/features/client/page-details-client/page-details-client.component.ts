51a789db34075d122f43ad46911e8c22e23adbc9
Absink <absink@absink.fr>
2023-11-21 11:45:06 +0100

    Add exo client model

--- src/app/features/client/page-details-client/page-details-client.component.ts
+++ src/app/features/client/page-details-client/page-details-client.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from 'src/app/shared/models/client.model';
 
 @Component({
   selector: 'app-page-details-client',
@@ -9,6 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PageDetailsClientComponent implements OnInit {
 
   public idClient!: string | null;
+  public description: string = "Id du client introuvable";
 
   constructor(
     private route: ActivatedRoute,
@@ -17,11 +19,29 @@ export class PageDetailsClientComponent implements OnInit {
 
 
   ngOnInit(): void {
+
+    let c1 = new Client(); c1.id = 1; c1.name = "Client 1";
+    let c2 = new Client(); c2.id = 2; c2.name = "Client 2";
     this.idClient = this.route.snapshot.paramMap.get("id");
+    switch(Number(this.idClient)) {
+      case 1:
+        this.description = `Client ID = ${c1.id} / Client Nom = ${c1.name}`;
+        break;
+      case 2:
+        this.description = `Client ID = ${c2.id} / Client Nom = ${c2.name}`;
+        break;
+      default:
+        this.description = "Id du client introuvable";
+        break;
+    }
   }
 
   public redirect(): void {
     this.router.navigateByUrl('clients');
+  }
+
+  public next(): void {
+    // this.router.navigateByUrl(`clients/${Number(this.idClient)+1}`);
   }
 
 }
